72befd1c1534eb4d0efc9799e4b6e0bc68cf270e
Nicolas Bouché <nicolas.bouche@univ-lyon1.fr>
2021-12-31 09:28:33 +0100

    adding posterior quantiles to plot

--- src/hyperfit/data.py
+++ src/hyperfit/data.py
@@ -75,7 +75,8 @@ class FitData(ABC):
 
         # Make the plot
         fig = plt.figure(1)
-        ax = fig.add_axes([0.15, 0.15, 0.95, 0.83])
+        fig.clf()
+        ax = fig.add_axes([0.15, 0.15, 0.8, 0.83])
         for i, e in enumerate(ells):
             ax.add_artist(e)
             if linfit is not None:
@@ -88,6 +89,12 @@ class FitData(ABC):
             ax.plot(xvals, yvals, c="k", marker="None", ls="-", lw=1.3, alpha=0.9)
             ax.plot(xvals, yvals - linfit.vert_scat, c="k", marker="None", ls="--", lw=1.3, alpha=0.9)
             ax.plot(xvals, yvals + linfit.vert_scat, c="k", marker="None", ls="--", lw=1.3, alpha=0.9)
+
+            y_bestfit = np.outer(xvals, linfit.coords[0]) + linfit.coords[1]
+            y_chain = np.outer(xvals, linfit.mcmc_samples[0]) + linfit.mcmc_samples[1]
+            y_chain_quantiles = np.quantile(y_chain, [0.1587, 0.8414], axis=1)
+            ax.fill_between(xvals, y_chain_quantiles[0], y_chain_quantiles[1], color="k", alpha=0.5)
+
         ax.set_xlabel(labels[0], fontsize=16)
         ax.set_ylabel(labels[1], fontsize=16)
         ax.set_xlim(limits[0][0], limits[0][1])
@@ -186,7 +193,7 @@ class Hogg(FitData):
             r"$\mathrm{log_{10}}(\mathrm{Stellar\,Mass}/M_{\odot})})$",
             r"$\mathrm{log_{10}}(R_{e}/\mathrm{kpc})$",
         ]
-        limits = [[45,350], [140, 700]]
+        limits = [[25,350], [140, 700]]
         self._plot_instance(labels, limits, linfit=linfit)
 
 
