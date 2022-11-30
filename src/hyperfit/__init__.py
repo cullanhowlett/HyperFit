736fee083c103e04cfab245c0da69f1415a8fed6
Cullan Howlett <cullan.howlett@gmail.com>
2022-11-30 10:53:15 -0800

    Doco fix by updating requirements.txt

--- src/hyperfit/__init__.py
+++ src/hyperfit/__init__.py
@@ -3,5 +3,5 @@
 # from .linfit import LinFit
 # from .data import FitData, GAMAsmVsize, ExampleData, TFR, FP6dFGS, MJB
 
-__version__ = "0.1.5"
+__version__ = "0.1.7"
 # __all__ = ["LinFit", "FitData", "GAMAsmVsize", "ExampleData", "TFR", "FP6dFGS", "MJB"]
